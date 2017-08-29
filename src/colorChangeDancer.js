04477b713a9018596d910defb2be4116b8e902ae
tylertruong <tylertruong@gmail.com>
2017-08-28 18:12:59 -0700

    Refactor makeDancer and makeBlinkyDancer and add new dancer types and methods  - Refactored previous makeDancer and makeBlinkyDancer  - Fixed makeBlinkyDancer tests to work with refactor (since we no longer call the Dancer.prototype.set method unnecessarily)  - Added colorChangeDancer, which is a dancer type that changes colour  - Added slideDance, which is a dance type that slides around  - Added lineUp method to the makeDancer class

--- src/colorChangeDancer.js
+++ src/colorChangeDancer.js
@@ -1,4 +1,4 @@
-var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
+var makeColorChangeDancer = function(top, left, timeBetweenSteps) {
   makeDancer.call(this, top, left, timeBetweenSteps);
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
@@ -10,14 +10,19 @@ var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
 
 };
 
-makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
-makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
+makeColorChangeDancer.prototype = Object.create(makeDancer.prototype);
+makeColorChangeDancer.prototype.constructor = makeColorChangeDancer;
 
-makeBlinkyDancer.prototype.step = function() {
+makeColorChangeDancer.prototype.step = function() {
   // call the old version of step at the beginning of any call to this new version of step
   makeDancer.prototype.step.call(this, this.timeBetweenSteps);
   // toggle() is a jQuery method to show/hide the <span> tag.
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
-  this.$node.toggle();
+  this.$node.toggleClass('dancerTurnBlue');
+};
+
+makeColorChangeDancer.prototype.danceWithPartner = function(otherDancer) {
+  this.$node.addClass('colorChangeDancerEffect');
+  otherDancer.$node.addClass('colorChangeDancerEffect');
 };
--- src/init.js
+++ src/init.js
@@ -22,12 +22,22 @@ $(document).ready(function() {
 
     // make a dancer with a random position
 
-    var dancer = dancerMakerFunction(
+    var dancer = new dancerMakerFunction(
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
       Math.random() * 1000
     );
     $('body').append(dancer.$node);
+    window.dancers.push(dancer);
   });
+
+  $('.lineUpButton').on('click', function(event) {
+    window.dancers.forEach(function(dancer, index) {
+      dancer.lineUp(index * 30);
+    });
+  });
+
+
+
 });
 
